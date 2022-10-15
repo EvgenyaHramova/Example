af642800167792d87347aab05a67f4b9e0278fa9
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-10-15 16:22:00 +0400

    izm Exs_02_HelloUser

--- Exs_02_HelloUser/Program.cs
+++ Exs_02_HelloUser/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите ваше имя: ");
+﻿Console.Write("Введите ваше имя: ");
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.Write(username);
